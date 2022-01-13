304c7025da09fc465fe8c7e8710d13675f841884
Hongye Liang <hongye_liang@cs.ucsb.edu>
2022-01-13 10:41:27 -0800

    car constructor

--- car.cpp
+++ car.cpp
@@ -4,4 +4,27 @@
 #include "car.hpp"
 #include <cstddef>
 #include <cstring>
+#include <iostream>
 // #include .......
+
+Car::Car(){
+    this->manufacturer = NULL;
+    this->model = NULL;
+    this->zeroToSixtyNs = 0;
+    this->headonDragCoeff = 0;
+    this->horsepower = 0;
+    this->backseatDoors = None; 
+    this->seatCount = 0;
+}
+
+Car(char const* const manufacturerName, char const* const modelName, PerformanceStats perf, uint8_t numSeats, DoorKind backseatDoorDesign){
+    manufacturer = manufacturerName;
+    model = modelName;
+    zeroToSixtyyNs = perf.zeroToSixtyyNs;
+    headonDragCoeff = perf.headonDragCoeff;
+    horsepower = perf.horsepower;
+    seatCount = numSeats;
+    backseatDoors = backseatDoorDesign;
+}
+
+
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include "car.hpp"
+#include <iostream>
+using namespace std
+
+int main(){
+    cout << "Testing" << endl;
+
+    Car c;
+    cout << c.getManufacturer;
+} 
+
